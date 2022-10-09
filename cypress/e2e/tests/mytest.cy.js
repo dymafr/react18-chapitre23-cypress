5bbdbb76aee1c126e174e8c9b517fa9d0ae72ed4
unknown <quentindevial19@gmail.com>
2022-10-09 11:45:09 +0200

    end chapter

--- cypress/e2e/tests/mytest.cy.js
+++ cypress/e2e/tests/mytest.cy.js
@@ -1,15 +1,55 @@
+/// <reference types="cypress" />
+
+import todos from "../../fixtures/todos";
+
+const BASE_URL = "https://restapi.fr/api/rtodocy";
+
 describe("my first set of tests", () => {
-  it("my first test", () => {
+  before(() => {
+    cy.intercept(BASE_URL, { fixture: "todos" }).as("fetchedTodos");
+    cy.fixture("todos").as("todos");
     cy.visit("/");
-    cy.contains(/list/);
-    cy.contains("h1", /list/);
-    cy.findByText(/list/i);
-    cy.get("h1");
-    cy.get(".container")
-      .find("h1")
-      .should("be.a", "object")
-      .and("have.class", "mb-20")
-      .and("have.css", "background-color", "rgb(255, 0, 0)")
-      .and("have.attr", "id", "unid");
+  });
+
+  it("should edit todo", () => {
+    cy.intercept("PATCH", `${BASE_URL}/${todos[0]._id}`, {
+      ...todos[0],
+      edit: true,
+    }).as("editTodo");
+
+    cy.contains(todos[0].content)
+      .parent()
+      .contains(/modifier/i)
+      .click();
+
+    cy.wait("@editTodo").its("request.body").should("deep.equal", {
+      content: todos[0].content,
+      edit: true,
+      done: false,
+    });
+
+    const type = " et une poire";
+
+    cy.intercept("PATCH", `${BASE_URL}/${todos[0]._id}`, {
+      ...todos[0],
+      content: todos[0].content + type,
+      edit: false,
+    }).as("savedTodo");
+
+    cy.findByDisplayValue(todos[0].content)
+      .type(type)
+      .parent()
+      .contains(/sauvegarder/i)
+      .click();
+
+    cy.wait("@savedTodo")
+      .its("request.body")
+      .should("deep.equal", {
+        content: todos[0].content + type,
+        edit: false,
+        done: false,
+      });
+
+    cy.contains(todos[0].content + type).should("exist");
   });
 });
--- src/App.js
+++ src/App.js
@@ -86,10 +86,8 @@ function App() {
   return (
     <div className="d-flex flex-row justify-content-center align-items-center p-20">
       <div className="card container p-20">
-        <h1 className="mb-20" id="unid" style={{ backgroundColor: "red" }}>
-          Todo list
-        </h1>
-        {/* <AddTodo addTodo={addTodo} />
+        <h1 className="mb-20">Todo list</h1>
+        <AddTodo addTodo={addTodo} />
         {loading ? (
           <p>Chargement en cours</p>
         ) : (
@@ -98,7 +96,7 @@ function App() {
             deleteTodo={deleteTodo}
             updateTodo={updateTodo}
           />
-        )} */}
+        )}
       </div>
     </div>
   );
